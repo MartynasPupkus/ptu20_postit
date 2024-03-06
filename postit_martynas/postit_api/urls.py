e5e4d95438d3cbab78e5e41f6517381b6a8d2b4b
MartynasPupkus <pupkus.martynas@gmail.com>
2024-03-06 11:25:10 +0200

    darbas su rest framework delete user

--- postit_martynas/postit_api/urls.py
+++ postit_martynas/postit_api/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('<int:pk>/comments/', views.CommentList.as_view()),
     path('comment/<int:pk>/', views.CommentDetail.as_view()),
-    path('<int:pk>/like/', views.PostLikeCreate.as_view())
+    path('<int:pk>/like/', views.PostLikeCreate.as_view()),
+    path('signup/', views.UserCreate.as_view()),
+    path('die/', views.UserDelete.as_view())
 ]
