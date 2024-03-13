d02760a4ec97f60829758fd58c8a83b7823f2508
MartynasPupkus <pupkus.martynas@gmail.com>
2024-03-13 13:49:38 +0200

    pasikeitimai

--- ptu20_postit/ptu20_postit/settings.py
+++ ptu20_postit/ptu20_postit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,9 +79,17 @@ WSGI_APPLICATION = 'ptu20_postit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     "ENGINE": "django.db.backends.postgresql",
+    #     "HOST": "postgres",
+    #     "NAME": os.getenv("POSTGRES_DB", "ptu20postit"),
+    #     "USER": os.getenv("POSTGRES_USER", "ptu20"),
+    #     "PASSWORD": os.getenv("POSTGRES_PASSWORD",""),
+    #     "PORT": os.getenv("POSTGRES_PORT", 5432),
+    # }
 }
 
 
